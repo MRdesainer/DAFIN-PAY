d4ff43600cb2ee80a34ef7caa44ed4f4141d381b
MRdesainer <nazilok618@gmail.com>
2026-08-05 01:34:01 +0700

    Amankan masuk kasir: wajib password admin dari halaman pembuka

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker - Kasir & Tabungan Barokah
 // Ganti "CACHE" versi di bawah ini setiap kali Anda memperbarui app
 // agar pengguna mendapat versi baru (cache ikut ter-refresh).
-const CACHE = 'barokah-v2';
+const CACHE = 'barokah-v3';
 
 const PRECACHE = [
     './index.html',
