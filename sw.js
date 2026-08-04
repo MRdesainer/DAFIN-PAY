bdc00f7d67bca6706510df99baaf82ed2c7a44e2
MRdesainer <nazilok618@gmail.com>
2026-08-05 01:49:32 +0700

    Pisahkan aplikasi kasir & wali: manifest terpisah + kunci masuk pada kasir.html

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker - Kasir & Tabungan Barokah
 // Ganti "CACHE" versi di bawah ini setiap kali Anda memperbarui app
 // agar pengguna mendapat versi baru (cache ikut ter-refresh).
-const CACHE = 'barokah-v4';
+const CACHE = 'barokah-v5';
 
 const PRECACHE = [
     './index.html',
@@ -9,6 +9,8 @@ const PRECACHE = [
     './wali.html',
     './config.js',
     './manifest.json',
+    './manifest-kasir.json',
+    './manifest-wali.json',
     './icons/icon-192.png',
     './icons/icon-512.png'
 ];
