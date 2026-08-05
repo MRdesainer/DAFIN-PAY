7b57ce1bdf7066c9c20ec1cff560dc04c560c4df
MRdesainer <nazilok618@gmail.com>
2026-08-05 11:25:48 +0700

    Tambah suara sukses + ucapan 'Terima kasih, barakallah' saat transaksi berhasil (belanja/tabungan/pembayaran wali) + pengaturan on/off; bump cache ke v18

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker - Kasir & Tabungan Barokah
 // Ganti "CACHE" versi di bawah ini setiap kali Anda memperbarui app
 // agar pengguna mendapat versi baru (cache ikut ter-refresh).
-const CACHE = 'barokah-v17';
+const CACHE = 'barokah-v18';
 
 const PRECACHE = [
     './index.html',
