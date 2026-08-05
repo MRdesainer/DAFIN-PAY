357f0620c5f77ea7883d9af5f5129212e79829ed
MRdesainer <nazilok618@gmail.com>
2026-08-05 08:13:28 +0700

    Hapus menu Portal Orang Tua di kasir; bump cache service worker ke v11

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker - Kasir & Tabungan Barokah
 // Ganti "CACHE" versi di bawah ini setiap kali Anda memperbarui app
 // agar pengguna mendapat versi baru (cache ikut ter-refresh).
-const CACHE = 'barokah-v10';
+const CACHE = 'barokah-v11';
 
 const PRECACHE = [
     './index.html',
